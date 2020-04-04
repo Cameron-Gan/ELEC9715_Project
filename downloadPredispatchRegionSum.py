ddd47ae64644129d3994baf0d9573df15924bb1b
Cameron-Gan <camerongan@gmail.com>
2020-04-04 16:19:41 +1100

    Scrapped out the most recent PredispatchRegionSum table

--- downloadPredispatchRegionSum.py
+++ downloadPredispatchRegionSum.py
@@ -1,15 +1,17 @@
 import requests
 import re
+import pandas as pd
+
 
 def downloadPredispatchRegionSum():
     url = 'http://www.nemweb.com.au/REPORTS/CURRENT/PredispatchIS_Reports/'
     r = requests.get(url)
     r = r.text.split(sep="<br>")
     latest = r[len(r) - 2]
-    print(latest)
-    latesturl = re.search(r'\"[^\"]*\"', latest)
-    print(latesturl(1))
-    
+    latesturl = re.search(r'\"([^\"]*)\"', latest).group(1)
+
+    zipfile = requests.get('http://www.nemweb.com.au'+latesturl)
+
 
 if __name__ == "__main__":
     downloadPredispatchRegionSum()
